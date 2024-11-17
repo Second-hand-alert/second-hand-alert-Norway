173fa3b7fd3f1c9c47f0594ce20ca3928ee1e372
eklem <espen.klem@gmail.com>
2024-11-17 19:41:07 +0100

    Cleanup of polarnopyret eventListener + regular matching

--- cs.polarnopyret.no.js
+++ cs.polarnopyret.no.js
@@ -1,59 +1,40 @@
-let titleTag = document.getElementsByTagName("title")[0].innerHTML;
-let regexPolarnopyretNo = RegExp('.+(?=-)', 'gu');
-
-let title = regexPolarnopyretNo.exec(titleTag)
-
+let titleTag = ''
+let regexPolarnopyretNo = ''
+let title = ''
+let currentUrl = location.href;
 let firstTime = true
 
+// Function for getting URL and title from page
+const getUrlTitle= function () {
+  console.log('url changed: ' + location.href)
+  titleTag = document.getElementsByTagName("title")[0].innerHTML
+  regexPolarnopyretNo = RegExp('.+(?=-)', 'gu')
+  title = regexPolarnopyretNo.exec(titleTag)
+  alert('Hallo Polarn O. Pyret side: ' + title + '\n' + location.href + '\nFørste POP-sidevisning?: ' + firstTime)
+} 
+
+// Get URL and title of page the first time entering polarnopyret.no
 if (firstTime === true) {
   setTimeout(function () {
-    console.log('url changed: ' + location.href);
-    titleTag = document.getElementsByTagName("title")[0].innerHTML;
-    regexPolarnopyretNo = RegExp('.+(?=-)', 'gu');
-    title = regexPolarnopyretNo.exec(titleTag)
-    alert('Hallo Polarn O. Pyret side: ' + title + '\n' + location.href + '\nOg en POP-side ble akkurat lastet')
-  }, 500); // Delay of 1.5 seconds
+    getUrlTitle()
+    firstTime = false
+  }, 500) // Delay of 1.5 seconds
 }
 
-// console.log('Hallo Polarn O. Pyret produkt-side for: ' + title)
-// alert('Hallo Polarn O. Pyret produkt-side for: ' + title)
-
-// window.addEventListener("click", notifyExtension);
-
-// function notifyExtension(e) {
-//   alert('Hallo Polarn O. Pyret side: \n' + title)
-// }
-
-// window.addEventListener('load', function () {
-//   let titleTag = document.getElementsByTagName("title")[0].innerHTML;
-//   let regexPolarnopyretNo = RegExp('.+(?=-)', 'gu');
-
-//   let title = regexPolarnopyretNo.exec(titleTag)
-//   alert('It\'s loaded!\n' + 'Hallo Polarn O. Pyret side: \n' + title)
-// })
-
-// alert('It\'s loaded!\n' + 'Hallo Polarn O. Pyret!')
-
-let currentUrl = location.href;
+// If event, check if transition is a "new page", meaning the URL has been rewritten
 const checkPageTransition = () => {
     requestAnimationFrame(() => {
         if (currentUrl !== location.href) {
           setTimeout(function () {
-            console.log('url changed: ' + location.href);
-            titleTag = document.getElementsByTagName("title")[0].innerHTML;
-            regexPolarnopyretNo = RegExp('.+(?=-)', 'gu');
-            title = regexPolarnopyretNo.exec(titleTag)
-            alert('Hallo Polarn O. Pyret side: ' + title + '\n' + location.href)
-          }, 500); // Delay of 1.5 seconds
+            getUrlTitle()
+          }, 500) // Delay of 1.5 seconds
         }
-        currentUrl = location.href;
-    }, true);
+        currentUrl = location.href
+    }, true)
 };
 
-
-document.body.addEventListener("click", checkPageTransition);
+// Event listeners
+document.body.addEventListener("click", checkPageTransition)
 document.body.addEventListener("keyup", e => {
     if ((e.code === "Enter" || e.code === "Space") && firstTime === false) checkPageTransition()
-});
-
-// alert('It\'s loaded!\n' + 'Hallo Polarn O. Pyret side: \n' + title)
+})
