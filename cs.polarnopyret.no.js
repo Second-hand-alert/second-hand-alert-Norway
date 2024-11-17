188c81b291640306c76cfe8da5b087a6e7d24a54
eklem <espen.klem@gmail.com>
2024-11-17 11:14:06 +0100

    Bug fixed, need some cleanup Both triggers should call one function (fistTime === true || eventListener) Also, needs a URL-checker. Do some regex to check that the page is actually a product page: https://www.polarnopyret.no/barneklaer/yttertoy/dresser/vanntett-vattert-dress-snowy-gronn-60601647-9010
    So in the end you have a number-number to check for.

--- cs.polarnopyret.no.js
+++ cs.polarnopyret.no.js
@@ -1,7 +1,19 @@
-// let titleTag = document.getElementsByTagName("title")[0].innerHTML;
-// let regexPolarnopyretNo = RegExp('.+(?=-)', 'gu');
+let titleTag = document.getElementsByTagName("title")[0].innerHTML;
+let regexPolarnopyretNo = RegExp('.+(?=-)', 'gu');
 
-// let title = regexPolarnopyretNo.exec(titleTag)
+let title = regexPolarnopyretNo.exec(titleTag)
+
+let firstTime = true
+
+if (firstTime === true) {
+  setTimeout(function () {
+    console.log('url changed: ' + location.href);
+    titleTag = document.getElementsByTagName("title")[0].innerHTML;
+    regexPolarnopyretNo = RegExp('.+(?=-)', 'gu');
+    title = regexPolarnopyretNo.exec(titleTag)
+    alert('Hallo Polarn O. Pyret side: ' + title + '\n' + location.href + '\nOg en POP-side ble akkurat lastet')
+  }, 500); // Delay of 1.5 seconds
+}
 
 // console.log('Hallo Polarn O. Pyret produkt-side for: ' + title)
 // alert('Hallo Polarn O. Pyret produkt-side for: ' + title)
@@ -28,9 +40,9 @@ const checkPageTransition = () => {
         if (currentUrl !== location.href) {
           setTimeout(function () {
             console.log('url changed: ' + location.href);
-            let titleTag = document.getElementsByTagName("title")[0].innerHTML;
-            let regexPolarnopyretNo = RegExp('.+(?=-)', 'gu');
-            let title = regexPolarnopyretNo.exec(titleTag)
+            titleTag = document.getElementsByTagName("title")[0].innerHTML;
+            regexPolarnopyretNo = RegExp('.+(?=-)', 'gu');
+            title = regexPolarnopyretNo.exec(titleTag)
             alert('Hallo Polarn O. Pyret side: ' + title + '\n' + location.href)
           }, 500); // Delay of 1.5 seconds
         }
@@ -41,7 +53,7 @@ const checkPageTransition = () => {
 
 document.body.addEventListener("click", checkPageTransition);
 document.body.addEventListener("keyup", e => {
-    if (e.code === "Enter" || e.code === "Space") checkPageTransition()
+    if ((e.code === "Enter" || e.code === "Space") && firstTime === false) checkPageTransition()
 });
 
 // alert('It\'s loaded!\n' + 'Hallo Polarn O. Pyret side: \n' + title)
