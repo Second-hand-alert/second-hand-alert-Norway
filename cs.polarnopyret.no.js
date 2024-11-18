e3e33941810a804ca8cc97a707d635b7160fe361
eklem <espen.klem@gmail.com>
2024-11-18 20:13:30 +0100

    cs.polarnopyret.no.js good enough for now

--- cs.polarnopyret.no.js
+++ cs.polarnopyret.no.js
@@ -1,24 +1,19 @@
-let titleTag = ''
-let regexPolarnopyretNo = ''
 let productPage = false
 const productPageRegex = /\d{5,10}-\d{3,5}$/gu
 let title = ''
-let currentUrl = location.href;
+let currentUrl = location.href
 let firstTime = true
 
 
 // Function for getting URL and title from page
 const getUrlTitle= function () {
   console.log('url changed: ' + location.href)
-  titleTag = document.getElementsByTagName("title")[0].innerHTML
-  regexPolarnopyretNo = RegExp('.+(?=-)', 'gu')
-  title = regexPolarnopyretNo.exec(titleTag)
+  title = document.getElementsByTagName("title")[0].innerHTML
   if (productPageRegex.test(location.href)) {
     productPage = true
   } else {
     productPage = false
   }
-  
   alert('Hallo Polarn O. Pyret side: ' + title + '\n' + location.href + '\nFørste POP-sidevisning: ' + firstTime + '\nProduct page: ' + productPage)
 } 
 
@@ -27,7 +22,7 @@ if (firstTime === true) {
   setTimeout(function () {
     getUrlTitle()
     firstTime = false
-  }, 500) // Delay of 1.5 seconds
+  }, 800) // Delay to get page title loaded
 }
 
 // If event, check if transition is a "new page", meaning the URL has been rewritten
@@ -36,13 +31,13 @@ const checkPageTransition = () => {
         if (currentUrl !== location.href) {
           setTimeout(function () {
             getUrlTitle()
-          }, 500) // Delay of 1.5 seconds
+          }, 800) // Delay to get page title loaded
         }
         currentUrl = location.href
     }, true)
 };
 
-// Event listeners
+// Event listeners for mouse and keyboard events
 document.body.addEventListener("click", checkPageTransition)
 document.body.addEventListener("keyup", e => {
     if ((e.code === "Enter" || e.code === "Space") && firstTime === false) checkPageTransition()
