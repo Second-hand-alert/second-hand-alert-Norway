bcaa5aa3334831b17fae6052bf56a79b57f0804e
eklem <espen.klem@gmail.com>
2024-11-19 20:22:27 +0100

    Fixing bug #17 - regex not working fully

--- cs.ikea.no.js
+++ cs.ikea.no.js
@@ -1,7 +1,8 @@
 let titleTag = document.getElementsByTagName("title")[0].innerHTML
-let regexIkeaNoNo = RegExp('.+(?=,)', 'gu')
+let regexIkeaNoNo = /^.+?(?=, )/gu
+let title = ''
 
-let title = regexIkeaNoNo.exec(titleTag)
+title = regexIkeaNoNo.exec(titleTag)
 
 alert('Hallo Ikea produkt-side for: ' + title)
-
+console.dir(title)
