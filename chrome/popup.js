cd62cb3dfa619b8fc11c625a424fffd5d6e0e499
eklem <espen.klem@gmail.com>
2024-12-21 21:50:45 +0100

    First working version!

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,8 +1,31 @@
+let searchArr = []
+
+function format(arr) {
+  arr = arr.reverse()
+  let HTML = ``
+
+  for (let i = 0; i < arr.length; i++) {
+    let objHTML = `
+      <div class="searchItem">
+        <h2><a target="_blank" href="${arr[i].URL}">${arr[i].title}</a></h2>
+        <p class="searchHits">${arr[i].searchResults} treff p&aring; ${arr[i].searchSite} med s&oslash;k p&aring;:<br/>'${arr[i].queryPartReadable}'</p>
+      </div>
+    `
+    // console.log(arr[i]);
+    HTML +=(objHTML)
+
+    const list = document.getElementById('list')
+    list.innerHTML = HTML
+  }
+}
+
 // Trigger script when opened
-// chrome.browserAction.onClicked
 chrome.runtime.sendMessage({type: 'POPUP_OPEN'})
   .then((response) => {
     console.info("Popup-js received response from background: ", JSON.stringify(response, null, ' '))
+    return response
+  }).then((arr) => {
+    format(arr)
   })
   .catch((error) => {
     console.warn("Popup.js could not send message to background", error)
