8ac4f47aa97345d6c7d55b1953233a7311cafbc9
eklem <espen.klem@gmail.com>
2025-08-26 21:55:59 +0200

    Fixing Ikea measurement extraction bug Fixing issue #101

--- dist-unpackaged/firefox/cs.ikea.no.js
+++ dist-unpackaged/firefox/cs.ikea.no.js
@@ -22,7 +22,7 @@
   function extractAndPrepareIkea (prodObj) {
     const regexStandard = /^.+?(?=, )/gmu;
     const testMeasurement = /(\d+x\d+x\d+\scm)|(\d+x\d+\scm)|(\d+\scm)/gmu;
-    const regexMeasurement = /[\d+]+/gu;
+    const regexMeasurement = /[\d]+(?=x)|[\d]+(?=\scm)/gu;
 
     prodObj.title = document.getElementsByTagName('title')[0].innerHTML;
     prodObj.queryPartReadable = regexStandard.exec(prodObj.title);
