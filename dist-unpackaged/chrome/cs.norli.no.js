d320838a148c15260d6189c4a3c72bce7d193f78
eklem <espen.klem@gmail.com>
2025-08-27 13:23:41 +0200

    Norli-bug: Moving back to click/key-up triggers Fixing #82

--- dist-unpackaged/chrome/cs.norli.no.js
+++ dist-unpackaged/chrome/cs.norli.no.js
@@ -58,11 +58,77 @@
       });
   }
 
-  let firstTime = true;
+  // let firstTime = true
 
-  window.navigation.addEventListener('navigate', (event) => {
-    if (firstTime === false && productUrlCheck(regexNorliProductPage, event.destination.url)) {
-      console.log('###### location changed: ' + event.destination.url);
+  // window.navigation.addEventListener('navigate', (event) => {
+  //   if (firstTime === false && productUrlCheck(regexNorliProductPage, event.destination.url)) {
+  //     console.log('###### location changed: ' + event.destination.url)
+  //     setTimeout(() => {
+  //       let prodObj = prodObjNorli()
+  //       console.log('#### Norli prodObj now: ' + JSON.stringify(prodObj, null, 2))
+  //       prodObj = extractAndPrepareNorli(prodObj)
+  //       sendObj(prodObj)
+  //     }, 1200)
+  //   }
+  // })
+
+  // // Needed for first page loaded
+  // setTimeout(() => {
+  //   if (firstTime && productUrlCheck(regexNorliProductPage, window.location.href)) {
+  //     firstTime = false
+  //     let prodObj = prodObjNorli()
+  //     console.log('#### Norli prodObj now: ' + JSON.stringify(prodObj, null, 2))
+  //     prodObj = extractAndPrepareNorli(prodObj)
+  //     sendObj(prodObj)
+  //   }
+  // }, 1200)
+
+  // console.log('Content at Norli!')
+
+  let firstTime = true;
+  let currentUrl = window.location.href;
+
+  const newURL = function (location) {
+    if (currentUrl !== location) {
+      currentUrl = location;
+      console.log('new url!');
+      return true
+    } else {
+      console.log('old url!');
+      return false
+    }
+  };
+
+  document.body.addEventListener('keyup', (event) => {
+    setTimeout(() => {
+      console.log('###### event keyup registered: ' + window.location.href);
+      if (event.code === 'enter' && productUrlCheck(regexNorliProductPage, window.location.href) && firstTime === false && newURL(window.location.href) === true) {
+        console.log('###### location changed: ' + window.location.href);
+        let prodObj = prodObjNorli();
+        console.log('#### Norli prodObj now: ' + JSON.stringify(prodObj, null, 2));
+        prodObj = extractAndPrepareNorli(prodObj);
+        sendObj(prodObj);
+      }
+    }, 1200);
+  });
+
+  document.body.addEventListener('click', (event) => {
+    setTimeout(() => {
+      console.log('###### event click registered: ' + window.location.href);
+      if (productUrlCheck(regexNorliProductPage, window.location.href) && firstTime === false && newURL(window.location.href)) {
+        console.log('###### location changed: ' + window.location.href);
+        let prodObj = prodObjNorli();
+        console.log('#### Norli prodObj now: ' + JSON.stringify(prodObj, null, 2));
+        prodObj = extractAndPrepareNorli(prodObj);
+        sendObj(prodObj);
+      }
+    }, 1200);
+  });
+
+  // Needed for first page loaded
+  if (firstTime === true) {
+    firstTime = false;
+    if (productUrlCheck(regexNorliProductPage, window.location.href)) {
       setTimeout(() => {
         let prodObj = prodObjNorli();
         console.log('#### Norli prodObj now: ' + JSON.stringify(prodObj, null, 2));
@@ -70,18 +136,7 @@
         sendObj(prodObj);
       }, 1200);
     }
-  });
-
-  // Needed for first page loaded
-  setTimeout(() => {
-    if (firstTime && productUrlCheck(regexNorliProductPage, window.location.href)) {
-      firstTime = false;
-      let prodObj = prodObjNorli();
-      console.log('#### Norli prodObj now: ' + JSON.stringify(prodObj, null, 2));
-      prodObj = extractAndPrepareNorli(prodObj);
-      sendObj(prodObj);
-    }
-  }, 1200);
+  }
 
   console.log('Content at Norli!');
 
