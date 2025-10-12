63d2fe981df51ff9b7794cc9e0f4c8aec89e92ee
eklem <espen.klem@gmail.com>
2025-10-12 08:54:33 +0200

    All files and framework for childrens clothes category Also clothes for grownups on some of the sites

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 export default [
   {
-    // Chrome - popup
+    // ######### Chrome - popup #########
     input: './lib/chrome/popup.js',
     output: {
       name: 'sha', file: './dist-unpackaged/chrome/popup.js', format: 'umd', exports: 'named'
@@ -10,84 +10,119 @@ export default [
     plugins: [nodeResolve()]
   },
   {
-    // Chrome - background
+    // ######### Chrome - background #########
     input: './lib/chrome/background.js',
     output: {
       name: 'sha', file: './dist-unpackaged/chrome/background.js', format: 'umd', exports: 'named'
     }
   },
+  // ######### Chrome furniture stores #########
   {
-    // Chrome - content - Ikea
     input: './lib/chrome/cs.ikea.js',
     output: {
       name: 'sha', file: './dist-unpackaged/chrome/cs.ikea.js', format: 'umd', exports: 'named'
     }
   },
+  // ######### Chrome book stores #########
   {
-    // Chrome - content - Abe books
     input: './lib/chrome/cs.abebooks.js',
     output: {
       name: 'sha', file: './dist-unpackaged/chrome/cs.abebooks.js', format: 'umd', exports: 'named'
     }
   },
   {
-    // Chrome - content - AdLibris
     input: './lib/chrome/cs.adlibris.js',
     output: {
       name: 'sha', file: './dist-unpackaged/chrome/cs.adlibris.js', format: 'umd', exports: 'named'
     }
   },
   {
-    // Chrome - content - Akademika
     input: './lib/chrome/cs.akademika.js',
     output: {
       name: 'sha', file: './dist-unpackaged/chrome/cs.akademika.js', format: 'umd', exports: 'named'
     }
   },
   {
-    // Chrome - content - Amazon.com
     input: './lib/chrome/cs.amazoncom.js',
     output: {
       name: 'sha', file: './dist-unpackaged/chrome/cs.amazoncom.js', format: 'umd', exports: 'named'
     }
   },
   {
-    // Chrome - content - Amazon.co.uk
     input: './lib/chrome/cs.amazoncouk.js',
     output: {
       name: 'sha', file: './dist-unpackaged/chrome/cs.amazoncouk.js', format: 'umd', exports: 'named'
     }
   },
   {
-    // Chrome - content - Ark
     input: './lib/chrome/cs.ark.js',
     output: {
       name: 'sha', file: './dist-unpackaged/chrome/cs.ark.js', format: 'umd', exports: 'named'
     }
   },
   {
-    // Chrome - content - Bokkilden
     input: './lib/chrome/cs.bokkilden.js',
     output: {
       name: 'sha', file: './dist-unpackaged/chrome/cs.bokkilden.js', format: 'umd', exports: 'named'
     }
   },
   {
-    // Chrome - content - Norli
     input: './lib/chrome/cs.norli.js',
     output: {
       name: 'sha', file: './dist-unpackaged/chrome/cs.norli.js', format: 'umd', exports: 'named'
     }
   },
   {
-    // Chrome - content - Tronsmo
     input: './lib/chrome/cs.tronsmo.js',
     output: {
       name: 'sha', file: './dist-unpackaged/chrome/cs.tronsmo.js', format: 'umd', exports: 'named'
     }
   },
+  // ######### Chrome clothe stores #########
   {
-    // Firefox - popup
+    input: './lib/chrome/cs.barnashus.js',
+    output: {
+      name: 'sha', file: './dist-unpackaged/chrome/cs.barnashus.js', format: 'umd', exports: 'named'
+    }
+  },
+  {
+    input: './lib/chrome/cs.didriksons.js',
+    output: {
+      name: 'sha', file: './dist-unpackaged/chrome/cs.didriksons.js', format: 'umd', exports: 'named'
+    }
+  },
+  {
+    input: './lib/chrome/cs.gullkorn.js',
+    output: {
+      name: 'sha', file: './dist-unpackaged/chrome/cs.gullkorn.js', format: 'umd', exports: 'named'
+    }
+  },
+  {
+    input: './lib/chrome/cs.kappahl.js',
+    output: {
+      name: 'sha', file: './dist-unpackaged/chrome/cs.kappahl.js', format: 'umd', exports: 'named'
+    }
+  },
+  {
+    input: './lib/chrome/cs.kattnakken.js',
+    output: {
+      name: 'sha', file: './dist-unpackaged/chrome/cs.kattnakken.js', format: 'umd', exports: 'named'
+    }
+  },
+  {
+    input: './lib/chrome/cs.polarnopyret.js',
+    output: {
+      name: 'sha', file: './dist-unpackaged/chrome/cs.polarnopyret.js', format: 'umd', exports: 'named'
+    }
+  },
+  {
+    input: './lib/chrome/cs.reima.js',
+    output: {
+      name: 'sha', file: './dist-unpackaged/chrome/cs.reima.js', format: 'umd', exports: 'named'
+    }
+  },
+  {
+    // ######### Firefox - popup #########
     input: './lib/firefox/popup.js',
     output: {
       name: 'sha', file: './dist-unpackaged/firefox/popup.js', format: 'umd', exports: 'named'
@@ -95,80 +130,115 @@ export default [
     plugins: [nodeResolve()]
   },
   {
-    // Firefox - background
+    // ######### Firefox - background #########
     input: './lib/firefox/background.js',
     output: {
       name: 'sha', file: './dist-unpackaged/firefox/background.js', format: 'umd', exports: 'named'
     }
   },
+  // ######### Firefox furniture stores #########
   {
-    // Firefox - content - Ikea
     input: './lib/firefox/cs.ikea.js',
     output: [
       { name: 'sha', file: './dist-unpackaged/firefox/cs.ikea.js', format: 'umd', exports: 'named' }
     ]
   },
+  // ######### Firefox books stores #########
   {
-    // Firefox - content - Abe books
     input: './lib/firefox/cs.abebooks.js',
     output: [
       { name: 'sha', file: './dist-unpackaged/firefox/cs.abebooks.js', format: 'umd', exports: 'named' }
     ]
   },
   {
-    // Firefox - content - AdLibris
     input: './lib/firefox/cs.adlibris.js',
     output: [
       { name: 'sha', file: './dist-unpackaged/firefox/cs.adlibris.js', format: 'umd', exports: 'named' }
     ]
   },
   {
-    // Firefox - content - Akademika
     input: './lib/firefox/cs.akademika.js',
     output: {
       name: 'sha', file: './dist-unpackaged/firefox/cs.akademika.js', format: 'umd', exports: 'named'
     }
   },
   {
-    // Firefox - content - Amazon.com
     input: './lib/firefox/cs.amazoncom.js',
     output: [
       { name: 'sha', file: './dist-unpackaged/firefox/cs.amazoncom.js', format: 'umd', exports: 'named' }
     ]
   },
   {
-    // Firefox - content - Amazon.couk
     input: './lib/firefox/cs.amazoncouk.js',
     output: [
       { name: 'sha', file: './dist-unpackaged/firefox/cs.amazoncouk.js', format: 'umd', exports: 'named' }
     ]
   },
   {
-    // Firefox - content - Ark
     input: './lib/firefox/cs.ark.js',
     output: [
       { name: 'sha', file: './dist-unpackaged/firefox/cs.ark.js', format: 'umd', exports: 'named' }
     ]
   },
   {
-    // Firefox - content - Bokkilden
     input: './lib/firefox/cs.bokkilden.js',
     output: {
       name: 'sha', file: './dist-unpackaged/firefox/cs.bokkilden.js', format: 'umd', exports: 'named'
     }
   },
   {
-    // Firefox - content - Norli
     input: './lib/firefox/cs.norli.js',
     output: [
       { name: 'sha', file: './dist-unpackaged/firefox/cs.norli.js', format: 'umd', exports: 'named' }
     ]
   },
   {
-    // Firefox - content - Tronsmo
     input: './lib/firefox/cs.tronsmo.js',
     output: [
       { name: 'sha', file: './dist-unpackaged/firefox/cs.tronsmo.js', format: 'umd', exports: 'named' }
     ]
+  },
+  // ######### Firefox clothe stores #########
+  {
+    input: './lib/firefox/cs.barnashus.js',
+    output: {
+      name: 'sha', file: './dist-unpackaged/firefox/cs.barnashus.js', format: 'umd', exports: 'named'
+    }
+  },
+  {
+    input: './lib/firefox/cs.didriksons.js',
+    output: {
+      name: 'sha', file: './dist-unpackaged/firefox/cs.didriksons.js', format: 'umd', exports: 'named'
+    }
+  },
+  {
+    input: './lib/firefox/cs.gullkorn.js',
+    output: {
+      name: 'sha', file: './dist-unpackaged/firefox/cs.gullkorn.js', format: 'umd', exports: 'named'
+    }
+  },
+  {
+    input: './lib/firefox/cs.kappahl.js',
+    output: {
+      name: 'sha', file: './dist-unpackaged/firefox/cs.kappahl.js', format: 'umd', exports: 'named'
+    }
+  },
+  {
+    input: './lib/firefox/cs.kattnakken.js',
+    output: {
+      name: 'sha', file: './dist-unpackaged/firefox/cs.kattnakken.js', format: 'umd', exports: 'named'
+    }
+  },
+  {
+    input: './lib/firefox/cs.polarnopyret.js',
+    output: {
+      name: 'sha', file: './dist-unpackaged/firefox/cs.polarnopyret.js', format: 'umd', exports: 'named'
+    }
+  },
+  {
+    input: './lib/firefox/cs.reima.js',
+    output: {
+      name: 'sha', file: './dist-unpackaged/firefox/cs.reima.js', format: 'umd', exports: 'named'
+    }
   }
 ]
